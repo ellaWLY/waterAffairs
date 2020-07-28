7381d4fd7a3edc372af9ae07b7c31d1799c0186b
ellaWLY <13820451821@163.com>
2020-07-28 17:43:51 +0800

    基础管理-组织信息

--- src/pages/basicsManage/tissueInfor.vue
+++ src/pages/basicsManage/tissueInfor.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tissue-box">
+    <div class="tissue-box" :style="{height: height+'px'}">
         <div class="tissue-tree">
             <div class="tissue-left">
                 <div class="tissue-title">
@@ -16,43 +16,73 @@
             <div class="tissue-right">
                 <div class="tissue-title">
                     <h3>组织信息</h3> 
-                    <span>
+                    <span v-if="appear">
                         <Button type="primary">保存</Button>
-                        <Button>取消</Button>
+                        <Button @click="cancel()">取消</Button>
                     </span>
                 </div>
                 <div class="tissue-content">
-                    <Form :model="tissueList" label-position="left" :label-width="100" autocomplete="off">
-                        <h4>组织名称</h4>
-                        <FormItem>
-                            <Input v-model="tissueList.name"></Input>
-                        </FormItem>
-                        <h4>组织类别</h4>
-                        <FormItem>
-                            <RadioGroup v-model="tissueList.type">
-                                <Radio label="company">公司</Radio>
-                                <Radio label="plant">工厂</Radio>
-                                <Radio label="department">部门</Radio>
-                                <Radio label="班组">班组</Radio>
-                            </RadioGroup>
-                        </FormItem>
-                        <div>公司信息</div>
-                        <FormItem label="公司全称：">
-                            <Input v-model="tissueList.fullName"></Input>
-                        </FormItem>
-                        <FormItem label="负责人：">
-                            <Input v-model="tissueList.principal"></Input>
-                        </FormItem>
-                        <FormItem label="手机：">
-                            <Input v-model="tissueList.phone"></Input>
-                        </FormItem>
+                    <Form :model="tissueList" label-position="right" :label-width="80" autocomplete="off">
+                        <div class="form-li">
+                            <h4>组织名称</h4>
+                            <FormItem :label-width="0" v-if="appear">
+                                <Input v-model="tissueList.name"></Input>
+                            </FormItem v-if="appearOther"rmItem>
+                            <div v-if="appearOther">联泰潮英智慧水务</div>
+                        </div>
+                        <div class="form-li">
+                            <h4>组织类别</h4>
+                            <FormItem :label-width="0" v-if="appear">
+                                <RadioGroup v-model="tissueList.type">
+                                    <Radio label="company">公司</Radio>
+                                    <Radio label="plant">工厂</Radio>
+                                    <Radio label="department">部门</Radio>
+                                    <Radio label="班组">班组</Radio>
+                                </RadioGroup>
+                            </FormItem>
+                        </div>
+                        <div class="form-box" v-if="appear">
+                            <div class="form-title">公司信息</div>
+                            <FormItem label="公司全称：">
+                                <Input v-model="tissueList.fullName"></Input>
+                            </FormItem>
+                            <FormItem label="负责人：">
+                                <Input v-model="tissueList.principal"></Input>
+                            </FormItem>
+                            <FormItem label="手机：">
+                                <Input v-model="tissueList.phone"></Input>
+                            </FormItem>
 
-                        <FormItem label="地址：">
-                            <Input v-model="tissueList.site"></Input>
-                        </FormItem>
-                        <FormItem label="附属信息：">
-                            <Input v-model="tissueList.other"></Input>
-                        </FormItem>
+                            <FormItem label="地址：">
+                                <Input v-model="tissueList.site"></Input>
+                            </FormItem>
+                            <FormItem label="附属信息：">
+                                <Input v-model="tissueList.other"></Input>
+                            </FormItem>
+                        </div>
+                        <div class="form-box form-other" v-if="appearOther">
+                            <div class="form-title">公司信息</div>
+                            <div class="other-li">
+                                <span>公司全称：</span>
+                                <span>联泰潮英智慧水务</span>
+                            </div> 
+                            <div class="other-li">
+                                <span>负责人：</span>
+                                <span>deht</span>
+                            </div>
+                            <div class="other-li">
+                                <span>手机：</span>
+                                <span>17180654515</span>
+                            </div>
+                            <div class="other-li">
+                                <span>地址：</span>
+                                <span></span>
+                            </div>
+                            <div class="other-li">
+                                <span>附属信息：</span>
+                                <span></span>
+                            </div>
+                        </div>
                     </Form>
                 </div>
             </div>
@@ -101,7 +131,9 @@ export default {
                                     display: data.is_show ? 'block' : 'none'
                                 },
                                 on: {
-                                    // click: () => { this.append(data) }
+                                    click: () => { 
+                                        this.newFun()
+                                    }
                                 }
                             },'新增')
                         ])
@@ -144,14 +176,21 @@ export default {
             },
             tissueList: {
                 name: '',
-                type: '',
+                type: 'company',
                 fullName: '',
                 principal: '',
                 phone: '',
                 site: '',
                 other: ''
-            }
+            },
+            appear: false,
+            appearOther: false,
+            height: ''
         }
+    },
+    mounted() {
+        console.log(document.body.clientHeight)
+        this.height = document.body.clientHeight-70
     },
     methods: {
         renderContent (h, { root, node, data }) {
@@ -219,6 +258,15 @@ export default {
             const parent = root.find(el => el.nodeKey === parentKey).node;
             const index = parent.children.indexOf(data);
             parent.children.splice(index, 1);
+        },
+        newFun() {
+            let self = this
+            self.appear = true
+        },
+        cancel() {
+            let self = this
+            self.appear = false
+            self.appearOther = true
         }
     }
 }
@@ -229,6 +277,7 @@ export default {
     .tissue-tree {
         display: flex;
         min-height: 100%;
+        height: 100%;
         .tissue-title {
             border-bottom: 1px solid #f0f0f0;
             height: 40px;
@@ -254,6 +303,7 @@ export default {
         .tissue-left {
             width: 350px;
             background: #FFF;
+            height: 100%;
             .tissue-list {
                 width: 100%;
                 padding: 0 10px;
@@ -265,12 +315,51 @@ export default {
         .tissue-right {
             flex: 1;
             min-height: 100%;
+            height: 100%;
             width: 100%;
             background: #FFF;
             border-left: 5px solid #f5f7f9;
             border-right: 5px solid #f5f7f9;
             .tissue-content {
                 padding: 20px 50px;
+                /deep/.ivu-input {
+                    width: 300px;
+                }
+                /deep/.ivu-form .ivu-form-item-label {
+                    padding-right: 0;
+                }
+                .form-li {
+                    border-bottom: 1px dashed #f0f0f0;
+                    h4 {
+                        height: 30px;
+                        margin-top: 10px;
+                    }
+                    /deep/.ivu-form-item {
+                        margin-bottom: 14px;
+                    }
+                }
+                .form-box {
+                    .form-title {
+                        background-color: #f4f4f4;
+                        height: 30px;
+                        line-height: 30px;
+                        text-indent: 20px;
+                        font-weight: 100;
+                        font-size: 13px;
+                        margin: 10px 0px;
+                    }
+                }
+                .form-other {
+                    .other-li {
+                        display: flex;
+                        font-size: 13px;
+                        line-height: 30px;
+                        span:nth-of-type(1) {
+                            width: 70px;
+                            text-align: right;
+                        }
+                    }
+                }
             }
         }
     }
